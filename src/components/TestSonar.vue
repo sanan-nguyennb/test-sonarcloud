706dcc28a90901242eb78ae06be50d0f5e8bb1cd
sanan-nguyennb <nguyennb@sanancorp.com>
2025-04-10 16:27:21 +0700

    force error

---
+++ src/components/TestSonar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: "TestSonar",
+  data() {
+    return {
+      message: "Hello",
+    };
+  },
+  created() {
+    eval("console.log('This is a security issue')");
+  },
+};
+</script>
